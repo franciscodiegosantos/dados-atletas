99ede1d5f7e23660e1da86d9a94a0020ec902b60
Diego Santos <132935254+franciscodiegosantos@users.noreply.github.com>
2025-11-09 19:08:29 -0300

    finalizado e testado o código

--- dados-atletas.js
+++ dados-atletas.js
@@ -37,30 +37,43 @@ class Atleta {
     }
 
     obtemNomeAtleta() {
-
+        return this.nome;
     }
 
     obtemIdadeAtleta() {
-
+        return this.idade;
     }
 
     obtemPesoAtleta() {
-
+        return this.peso;
     }
 
     obtemNotasAtleta() {
-
+        return this.notas;
     }
 
     obtemCategoria() {
-
+        return this.calculaCategoria();
     }
 
     obtemIMC() {
-
+        return this.calculaIMC();
     }
 
     obtemMediaValida() {
-
+        return this.calculaMediaValida();
     }
 }
+
+    let atleta = new Atleta("Cesar Abascal",
+    30, 80, 1.70,
+    [10, 9.34, 8.42, 10, 7.88]);
+
+    console.log("Nome: " + atleta.obtemNomeAtleta());
+    console.log("Idade: " + atleta.obtemIdadeAtleta());
+    console.log("Peso: " + atleta.obtemPesoAtleta());
+    console.log("Altura: " + atleta.altura);
+    console.log("Notas: " + atleta.obtemNotasAtleta());
+    console.log("Categoria: " + atleta.obtemCategoria());
+    console.log("IMC: " + atleta.obtemIMC());
+    console.log("Média Válida: " + atleta.obtemMediaValida());
